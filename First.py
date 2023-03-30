c80d938b29e21c247fcda225b8f5c5b689f5b726
mangeshwayaldeveloper <120046874+mangeshwayaldeveloper@users.noreply.github.com>
2023-03-30 21:05:39 +0530

    this is the day 3 commit of python project

--- First.py
+++ First.py
@@ -2,3 +2,13 @@ print("This is my First Python Project")
 a=235425
 print(a)
 print(345+234)
+print(type(a))
+
+#complex in python are the data types
+a=complex(8,2)
+print(a)
+#data types in python
+#Numeric
+#Text
+#Boolean
+#Sequence
