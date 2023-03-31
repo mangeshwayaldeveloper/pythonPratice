e2d77cfc74a93f8f923105494f3c6de67f5d1cf1
mangeshwayaldeveloper <120046874+mangeshwayaldeveloper@users.noreply.github.com>
2023-03-31 17:18:07 +0530

    this is the commit of calculator program``

---
+++ CalculatorOfTwoNum.py
@@ -0,0 +1,6 @@
+a=30
+b=10
+print("Addition of Two Number Is ",a+b)
+print("Subtraction of Two Number Is ",a-b)
+print("Multiplication Of Two Number Is",a*b)
+print("Division Of Two Numbers Is",a/b)
