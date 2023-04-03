a15dc5febfc16261366b9ab33c9030ca92cd58e4
mangeshwayaldeveloper <120046874+mangeshwayaldeveloper@users.noreply.github.com>
2023-04-03 16:47:04 +0530

    strings commit

--- StringMethods.py
+++ StringMethods.py
@@ -14,3 +14,21 @@ print(Str1.count(" "))
 print(Str1.find("is"))
 
 print(Str.index("am"))
+mail="mangeshwayal"
+print(mail.isalnum())
+print(mail.isalpha())
+st="    "
+print(st.islower())
+st="    \n"
+print("This returns ",st.isprintable())
+print(st.isspace())
+st2="The Every World Of The "
+print(st2.istitle())
+str3="WHAT IS MEANS"
+print(str3.isupper())
+str4="This is the test that every word starts with"
+print(str4.startswith("This"))
+python="Python Is Interpreted Language"
+print(python.swapcase())
+titl="i think this will make every word capitalised"
+print(titl.title())
