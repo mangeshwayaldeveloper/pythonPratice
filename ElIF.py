4dec35c3ff2c79bb881d724485398c2cc75948c5
mangeshwayaldeveloper <120046874+mangeshwayaldeveloper@users.noreply.github.com>
2023-04-03 17:14:01 +0530

    strings commit

---
+++ ElIF.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number you want \n"))
+if a > 10:
+    print("The Number is Greater than 10")
+elif  a>= 30:
+    print("The number is definately greater than 30")
+elif a > 50:
+    print("The number is greater than 50")
+else:
+    print("This number is invalid or negative")
+
+
+#in above program if condition becomes true then program returns that statement without checking the remaining statements
---
+++ IfElse.py
@@ -0,0 +1,7 @@
+a=int(input("Enter Your Age \n"))
+if(a>18):
+    str="your are allowed to drive"
+    print(str.title())
+else :
+    print("Your Are Not Allowed To Drive")
+print("This will execute every time")
