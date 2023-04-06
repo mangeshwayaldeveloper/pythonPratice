70fa9e4e773efd6020521a81e5a685fb70caa82f
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-04-06 15:49:38 +0530

    functions and loops

---
+++ FunctionArguments.py
@@ -0,0 +1,9 @@
+#default arguments function
+def name(a=43,b=6):
+    print("Average of the number is ",(a*b)/(a+b))
+
+name()
+#---------------------------------------------------------------
+def yourName(fname,mname="vithoba",lname="wayal"):
+    print(fname,mname,lname)
+yourName("mangesh")
---
+++ forLoopPractise.py
@@ -0,0 +1,8 @@
+fname={"mangesh","rushi","pavan"}
+for i in fname:
+    print(i)
+
+#---------------------------------------------------
+lname=["wayal","wable","kale"]
+for r in lname:
+    print(r)
