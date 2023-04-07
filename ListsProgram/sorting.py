774b57fd43a42e391725c33f6c51a44dc90b29fa
mangeshwayaldeveloper <mangeh.wayal.dypimr@gmail.com>
2023-04-07 16:00:49 +0530

    list methods

--- ListsProgram/sorting.py
+++ ListsProgram/sorting.py
@@ -1,6 +1,10 @@
-list=[1,4,2,3,5,6,7,3]
+list = [1, 4, 2, 3, 5, 6, 7, 3]
 list.sort()
 print(list)
 list.reverse()
 print(list)
-print(list.index(4))
+print('printing the index number of ', list.index(4))
+
+num = [4, 2, 5, 4, 6, 7, 3, 2, 4, 5, 6, 7]
+print(num.count(4))
+print(num.index(4))
