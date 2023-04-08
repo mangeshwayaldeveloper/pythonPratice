61324edd8d9703207e53fee4034ad7ec19a569f0
mangeshwayaldeveloper <mangeh.wayal.dypimr@gmail.com>
2023-04-08 16:35:09 +0530

    greeting Exercise program

--- GoodMorningSir.py
+++ GoodMorningSir.py
@@ -1,11 +1,12 @@
 import time
-timestamp=time.strftime('%H')
-a=int(22)
-if(a<16 & a>12):
-    print("Good Afternoon Sir",timestamp)
-elif(a>16 & a<21):
-    print("Good Evening Sir",timestamp)
-elif(a>21 & a<6):
-    print("Good Night Sir",timestamp)
+
+hours = int(time.strftime('%H'))
+
+if hours >= 0 and hours < 12:
+    print("Good Morning Sir", hours)
+elif hours >= 12 and hours < 17:
+    print("Good Evening Sir", hours)
+elif hours >= 17 and hours < 0:
+    print("Good Night Sir", hours)
 else:
     print("Good Morning Sir")
