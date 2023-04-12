989f799217166b63e3fd6d8a4662bfcdedc899b2
JADHAV ASHISH <77589461+AshishJadhav45@users.noreply.github.com>
2023-04-12 18:46:04 +0530

    Update IfElse.py

--- IfElse.py
+++ IfElse.py
@@ -1,7 +1,9 @@
+
+
 a=int(input("Enter Your Age \n"))
 if(a>18):
-    str="your are allowed to drive"
+    str="your are allowed to vote"
     print(str.title())
 else :
-    print("Your Are Not Allowed To Drive")
+    print("Your Are Not Allowed To voting")
 print("This will execute every time")
