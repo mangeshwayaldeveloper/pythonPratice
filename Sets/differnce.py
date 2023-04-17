68159a0a86f1466eadc1f918f90c303fa2a7d11a
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-04-17 19:40:24 +0530

    set and set methods

--- Sets/differnce.py
+++ Sets/differnce.py
@@ -1,3 +1,4 @@
 s={3,4,"after",46,3,4,"year"}
 s1={3,4,5,3,"o"}
 print(s.difference(s1))
+print(s1.difference_update(s))
---
+++ Sets/issuperset.py
@@ -0,0 +1,5 @@
+c1={4.3,56,8,4}
+c2={23,53,567,42}
+c3={23,53,567}
+print("c1 is super set of c2=>",c1.issuperset(c2))
+print('c2 is super set of c3=>',c2.issuperset(c3))
