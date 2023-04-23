9415252636b3b47cebd7b6e8de7c6eaf0ddc0b96
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-04-23 22:07:36 +0530

    function

---
+++ coursera/returnFunction.py
@@ -0,0 +1,6 @@
+def area_of_triangel(base, height):
+    return base*height/2
+area_a=area_of_triangel(3,4)
+area_b=area_of_triangel(6,3)
+sum=area_a+area_b
+print(sum)
---
+++ coursera/useOfFunction.py
@@ -0,0 +1,3 @@
+name="kay"
+number=len(name)*9
+print("Hello "+name+". Your lucky number is "+str())
