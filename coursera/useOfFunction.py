c18ea42d29d03e8485be186127698da09c4e99cb
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-04-25 19:05:52 +0530

    function

--- coursera/useOfFunction.py
+++ coursera/useOfFunction.py
@@ -1,3 +1,7 @@
 name="kay"
 number=len(name)*9
-print("Hello "+name+". Your lucky number is "+str())
+print("Hello "+name+". Your lucky number is "+str(number))
+
+name='inch'
+number=len(name)*9
+print("Hello "+name+" Your lucky number is "+str(number))
