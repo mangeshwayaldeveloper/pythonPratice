0680042579b281c91752fdbf3eba6afc2d023641
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-08 12:43:42 +0530

    base power

--- ListsProgram/append.py
+++ ListsProgram/append.py
@@ -2,3 +2,4 @@ color=['voiltet','indigo','blue']
 color.append('green')
 color.append("red")
 print(color)
+print(color[0])
---
+++ coursera/BasePower.py
@@ -0,0 +1,13 @@
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return base**0
+  elif number>base:
+    return base**3
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number/base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
