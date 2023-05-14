18962a13b69715db8288464ba71fa94d50fd7f1e
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-14 16:17:09 +0530

    enumerate function

---
+++ Enumerate_function/enumerate_function_approach.py
@@ -0,0 +1,5 @@
+marks=[23,3,21,32,45,21,98]
+for index, marks in enumerate(marks):
+    print(marks)
+    if (index==1):
+        print("You are failed")
---
+++ Enumerate_function/old_aaproach.py
@@ -0,0 +1,7 @@
+marks=[12,43,4,1,12,34,99,10,44,32,3]
+index=0
+for marks in marks:
+    print(marks)
+    if index==3:
+        print("your are awesome you got just "+str(marks)+" Mark")
+    index+=1
