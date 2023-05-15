34fc5d0fa74e8ee6143de53fd57df654c6c893c6
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-15 23:36:15 +0530

    enumerate function

--- Enumerate_function/enumerate_function_approach.py
+++ Enumerate_function/enumerate_function_approach.py
@@ -1,5 +1,5 @@
 marks=[23,3,21,32,45,21,98]
-for index, marks in enumerate(marks):
+for index, marks in enumerate(marks,start=1):
     print(marks)
     if (index==1):
         print("You are failed")
---
+++ multiplication_table.py
@@ -0,0 +1,6 @@
+def mult(num):
+    for i in range(11):
+        print(i*num)
+
+i=int(input("enter a num"))
+print(mult(i))
