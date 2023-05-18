56f42be4e950bdb8282c6e474c58b4d8cf7b6f01
mangeshwayaldeveloper <mangesh.wayal.dypimr@gmail.com>
2023-05-19 00:11:52 +0530

    prime num

--- primeNumberPro.py
+++ primeNumberPro.py
@@ -1,12 +1,15 @@
-num=int(input("Enter a number"))
-Flag=True
-if (num==0 or num==1):
+num = int(input("Enter a number"))
+Flag = False
+if (num == 0 or num == 1):
     print("The number is Niether prime or not non-prime")
-else:
-    for i in range(1,num):
-        if(num%i==0):
+elif num>1:
+    for i in range(2,num):
+        if (num % i == 0):
+            Flag = True
+            break
+        else:
             Flag=False
-if(Flag==True):
+if (Flag == False):
     print("Number is prime")
 else:
     print("Number is not prime")
