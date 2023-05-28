defa1f914ca95c16510ea37dde7c15fdbb6d0e13
My name <mangesh.wayal.dypimr@gmail.com>
2023-05-28 18:57:09 +0530

    class introduction

---
+++ OOPs/class_first.py
@@ -0,0 +1,13 @@
+class Apple:
+    color=""
+    flavor=""
+
+obj=Apple()
+obj.color="red"
+obj.flavor="sweet"
+
+print(obj.color.upper())
+print(obj.flavor)
+
+golden=Apple()
+golden.color=""
---
+++ OOPs/class_second.py
@@ -0,0 +1,13 @@
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color ="red"
+
+violet=Flower()
+violet.color='blue'
+this_pun_is_for_you =Flower()
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you)
