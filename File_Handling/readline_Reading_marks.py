9d585d1dc0b92ed0ccb8453d21c0ba883b3bc4e9
My name <mangesh.wayal.dypimr@gmail.com>
2023-05-29 22:28:49 +0530

    readline

--- File_Handling/readline_Reading_marks.py
+++ File_Handling/readline_Reading_marks.py
@@ -1,1 +1,13 @@
-f=open('marks.txt','x')
+f=open('marks.txt')
+i=0
+while True:
+    i=i+1
+    line=f.readline()
+    if not line:
+        break
+    m1=line.split(",")[0]
+    m2=line.split(",")[1]
+    m3=line.split(",")[2]
+    print(f"Marks of student {i} in Maths is: {m1}")
+    print(f"Marks of student {i} in English is: {m2}")
+    print(f"Marks of student {i} in Python is: {m3}")
