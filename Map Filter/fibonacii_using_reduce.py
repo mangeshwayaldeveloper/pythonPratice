98958b0f48ac890c3b15ae24fd9f28c61f841456
My name <mangesh.wayal.dypimr@gmail.com>
2023-06-01 23:23:58 +0530

    fibonacci program using reduce function

--- Map Filter/fibonacii_using_reduce.py
+++ Map Filter/fibonacii_using_reduce.py
@@ -1,8 +1,10 @@
 from functools import reduce
 
+l = []
 num = int(input("Enter a number"))
-for i in range(num):
- l = []
+for i in range(num+1):
+ l.append(i)
+
 
 fibo = reduce(lambda x, y: x + y, l)
 print(fibo)
