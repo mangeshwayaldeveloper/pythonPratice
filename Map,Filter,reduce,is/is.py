cbba77bf2be27ec10e30e2cce643df2df763c9bc
My name <mangesh.wayal.dypimr@gmail.com>
2023-06-01 23:38:53 +0530

    use of is

---
+++ Map,Filter,reduce,is/is.py
@@ -0,0 +1,15 @@
+a=4
+b=4
+print('if we compare two constant values using is ' ,a is b)
+print('if we compare two constant values using == ',a==b)
+
+print("------------------------------------------------------------")
+a=[1,2,4]
+b=[1,2,4]
+print('if we print two list using == ',a==b)
+print('if we print two list usign is ',a is b)
+print('-------------------------------------------------------------------------')
+a=(1,2,4)
+b=(1,2,4)
+print("if we print two tuples using is ",a is b)
+print("if we print two values using == ",a==b)
