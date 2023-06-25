1d21c37461a23d1e9363f14592819106f33e95bf
My name <mangesh.wayal.dypimr@gmail.com>
2023-06-25 12:10:54 +0530

    decorators

---
+++ Decoraters/deco_third.py
@@ -0,0 +1,12 @@
+def greet(fx):
+ def mxf():
+  print("Hello,Welcome to this function")
+  fx()
+  print("Thank You for Function")
+ return mxf
+
+@greet
+def Hello():
+ print("Hey there this is the hello function")
+
+Hello()
