9c02f2253dfb6906c8b33c4a6f1a4ef0a25528cf
My name <mangesh.wayal.dypimr@gmail.com>
2023-07-14 10:52:10 +0530

    creating classses

---
+++ Assingment/Assignment 2/Question11.py
@@ -0,0 +1,9 @@
+class cls:
+ def show(self,name,age):
+  print("This program will show the use of multiple objects")
+  print("The name of the person {} and age is {}".format(name,age))
+
+obj1=cls()
+obj1.show("John",32)
+obj2=cls()
+obj2.show("pintu",44)
